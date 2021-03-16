130c72dfca045429a20d9e2a1bfe7fa778aa8215
Satyam Mishra <mishrasatyam626@gmail.com>
2021-03-16 15:13:47 +0530

    full code

--- Floor&Ceil.cpp
+++ Floor&Ceil.cpp
@@ -1,10 +1,39 @@
 #include<iostream>
 using namespace std;
 
+void BSThelper(TreeNode* root, int key, int& floor, int& ceil)
+{
+    while(root)
+    {
+        if(key == root->val)
+        {
+            floor = root->val;
+            ceil = root->val;
+            return;
+        }
+        if(key > root->val)
+        {
+            floor = root->val;
+            root = root->right;
+        }
+        else{
+            ceil = root->val;
+            root = root->left;
+        }
+    }
+    return;
+}
 
 int main()
 {
 
-cout <<"Hello Git";
+void floorCeilBST(TreeNode* root, int key)
+{
+    int floor = -1, ceil = -1;
+    BSThelper(root, key, floor, ceil);
+    cout << key <<" " << floor <<" " << ceil;
+}
+
+
     return 0;
 }
